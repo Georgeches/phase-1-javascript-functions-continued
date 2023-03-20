2b7dba5ba6d87da3798f0ac72dea09abc32c179e
Georgeches <georgechesire83@gmail.com>
2023-03-20 17:07:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(arg){
+    if (!arg){
+        return "This Saturday, I want to roller-skate!"
+    }
+    else{
+        return `This Saturday, I want to ${arg}!`
+    }
+    
+}
+
+function mondayWork(arg){
+
+    if(!arg){
+        return "This Monday, I will go to the office."
+    }
+    else{
+        return "This Monday, I will work from home."
+    }
+}
+
+function print(a, b){
+    return `${a}${b}${a}`
+}
+
+function wrapAdjective(highlight, text){
+    return print(highlight, text)
+}
+
+console.log(wrapAdjective("||", "george"))
